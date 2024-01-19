7ab60b58b3caa319d1562bdef0063c8cd91bc6d9
btbrad <24836323+btbrad@users.noreply.github.com>
2024-01-19 21:26:29 +0800

    add tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <!-- <NuxtWelcome /> -->
-    <el-button @click="ElMessage('hello')">button</el-button>
+    <div class="w-full h-[100vh] flex justify-center items-center">
+      <el-button type="primary" @click="ElMessage('hello')">button</el-button>
+    </div>
   </div>
 </template>
