2e3a464cc7347478bfbba36328cf285304be3fc0
btbrad <24836323+btbrad@users.noreply.github.com>
2024-01-23 21:10:46 +0800

    feat: 登录逻辑

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@element-plus/nuxt', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@element-plus/nuxt',
+    '@nuxtjs/tailwindcss',
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: [
+          // 自动引入 `defineStore(), storeToRefs()`
+          'defineStore',
+          'storeToRefs',
+        ],
+      },
+    ],
+  ],
   elementPlus: {
     /** Options */
   },
--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+  import type { FormInstance, FormRules } from 'element-plus'
+  import { useUser } from '../store/user';
+
   useHead({
     title: '登录'
   })
@@ -6,6 +9,34 @@
   definePageMeta({
     layout: 'blank'
   })
+
+  const formRef = ref<FormInstance>()
+  const model = ref({
+    username: '',
+    password: '',
+  })
+
+  const rules: FormRules = {
+    username: [
+      { required: true, message: '请输入用户名', trigger: 'blur' }
+    ],
+    password: [
+      { required: true, message: '请输入密码', trigger: 'blur' }
+    ],
+  }
+
+  const store = useUser()
+  const login = async () => {
+    await formRef.value!.validate()
+    const { ok, data } = httpPost('/api/login', {
+      username: model.value.username,
+      password: model.value.password
+    })
+    if (ok) {
+      store.userInfo = data
+      navigateTo('/')
+    }
+  }
 </script>
 
 <template>
@@ -16,15 +47,15 @@
 
   <el-alert title="演示账号密码为：test" type="success" class="!mb-6" show-icon :closable="false" />
 
-  <el-form ref="formRef" class="w-[340px]" size="large">
+  <el-form ref="formRef" :model="model" :rules="rules" class="w-[340px]" size="large">
     <el-form-item prop="username">
-      <el-input placeholder="用户名" clearable />
+      <el-input v-model.value="model.username" placeholder="用户名" clearable />
     </el-form-item>
     <el-form-item prop="password">
-      <el-input type="password" placeholder="密码" clearable />
+      <el-input v-model.value="model.password" type="password" placeholder="密码" clearable />
     </el-form-item>
     <div>
-      <el-button class="w-full" type="primary">登录</el-button>
+      <el-button class="w-full" type="primary" @click="login">登录</el-button>
     </div>
   </el-form>
 </template>
